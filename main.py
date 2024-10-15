9ae5440f550a03a4f58a1b2cfab909037dd04223
OSVALDO GARCES C <osvaldogarcescardenas@gmail.com>
2024-10-15 12:22:38 -0500

    Actualizacion conexion

--- main.py
+++ main.py
@@ -11,22 +11,4 @@ if __name__ == '__main__':
     Interno = InternoController()
     Celda = CeldaController()
     conexion.conectar()
-
-    # Crear una nueva celda primero
-    Celda.crear_celda(conexion, "Medellin", 5, "Disponible")
-
-    # Luego crear un nuevo interno, utilizando el ID de la celda creada (asumimos que es 1 por simplicidad)
-    Interno.crear_interno(conexion, "Juan Perez", datetime.date(2023, 10, 1), "Activo", 1)
-
-
-    # Leer internos
-    #Interno.leer_internos(conexion)
-
-    # Actualizar un interno
-    #Interno.actualizar_interno(conexion, 1, "Liberado")
-
-    # Eliminar un interno
-    #Interno.eliminar_interno(conexion, 1)
-
-    # Cerrar la conexión
     conexion.cerrar()
