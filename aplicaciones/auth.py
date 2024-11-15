e974b730910927c0d06dad6a68f42aeb329961ef
Andrei Florez <Andrea Sanchez@LAPTOP-81DE834F>
2024-11-14 22:36:20 -0500

    Add authorization request

---
+++ aplicaciones/auth.py
@@ -0,0 +1,36 @@
+import datetime
+import jwt
+from flask import Blueprint, request, jsonify
+
+class AuthController:
+    def __init__(self, app):
+        auth_blueprint = Blueprint('auth', __name__)
+        auth_blueprint.add_url_rule('/token', view_func=self.token, methods=["POST"])
+        app.register_blueprint(auth_blueprint, url_prefix='/auth')
+
+    def token(self):
+        try:
+            datos = request.json
+            admin_user = 'admin_carcel'
+            admin_password = 'Qwer.1234'
+            key = 'KJhisdy8787798udfsd56f4s5d4fsdf'
+
+            # Validación de usuario y contraseña
+            if "User" not in datos or datos["User"] != admin_user:
+                return jsonify({"Error": "El usuario es incorrecto"}), 400
+            if "Password" not in datos or datos["Password"] != admin_password:
+                return jsonify({"Error": "La contraseña es incorrecta"}), 400
+
+            # Generación del token con expiración
+            encoded = jwt.encode(
+                {
+                    "Usuario": admin_user,
+                    "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=10)
+                },
+                key,
+                algorithm="HS256"
+            )
+            return jsonify({"Token": encoded, "Response": "Ok"}), 200
+
+        except Exception as ex:
+            return jsonify({"Error": str(ex)}), 400
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import flask;
+from flask import Flask, request, jsonify
+import jwt
 
 from common.conexion import Conexion
 from aplicaciones.celda import CeldaController
@@ -13,25 +14,45 @@ from aplicaciones.delito import DelitoController
 from aplicaciones.internoActividad import InternoActividadController
 from aplicaciones.condena import CondenaController
 from aplicaciones.informeDisciplina import InformeDisciplinaController
-
+from aplicaciones.auth import AuthController
 from aplicaciones.reglasN import ReglasNController
 
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "KJhisdy8787798udfsd56f4s5d4fsdf"
+
+@app.before_request
+def verify_token():
+    # Lista de rutas públicas que no requieren autenticación
+    public_routes = ['auth.token']  
+    
+    # Verificar si la ruta no es pública
+    if request.endpoint not in public_routes:
+        token = request.headers.get("Authorization")
+        print(token)
+        if not token:
+            return jsonify({"Error": "Token faltante"}), 401
+        try:
+            jwt.decode(token.replace('Bearer ', ''), app.config["SECRET_KEY"], algorithms=["HS256"])
+        except jwt.ExpiredSignatureError:
+            return jsonify({"Error": "Token expirado"}), 401
+        except jwt.InvalidTokenError:
+            return jsonify({"Error": "Token inválido"}), 401
+
+# Registro de los controladores
+CeldaController(app)
+InternoController(app)
+ActividadController(app)
+PersonalController(app)
+TransferenciaController(app)
+VisitaController(app)
+VisitanteController(app)
+VisitaMultipleController(app)
+DelitoController(app)
+InternoActividadController(app)
+InformeDisciplinaController(app)
+CondenaController(app)
+ReglasNController(app)
+AuthController(app)
 
 if __name__ == "__main__":
-    app = flask.Flask(__name__)
-    CeldaController(app)
-    InternoController(app)
-    ActividadController(app)
-    PersonalController(app)
-    TransferenciaController(app)
-    VisitaController(app)
-    VisitanteController(app)
-    VisitaMultipleController(app)
-    DelitoController(app)
-    InternoActividadController(app)
-    InformeDisciplinaController(app)
-    CondenaController(app)
-    ReglasNController(app)
-    app.run('localhost', 4040);
-
-
+    app.run('localhost', 4040)
